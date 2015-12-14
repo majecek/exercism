577b5a7e9a9f6717cfc1ae0e449dd4f15c0c1b59
Marek Chowaniok <marek.chowaniok@gmail.com>
2015-12-14 08:31:41 +0100

    WordCount in JDK8

--- java/word-count/src/main/java/WordCount.java
+++ java/word-count/src/main/java/WordCount.java
@@ -1,20 +1,31 @@
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.regex.Matcher;
+import java.util.function.Function;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class WordCount {
 
     private final Map<String, Integer> splitMap = new HashMap<>();
 
     public Map<String, Integer> Phrase(String word) {
-        Pattern pattern = Pattern.compile("\\w+");
-        Matcher matcher = pattern.matcher(word.toLowerCase());
+        final Pattern pattern = Pattern.compile("\\w+");
 
-        while (matcher.find()) {
-            int wordCount = splitMap.get(matcher.group()) != null ? splitMap.get(matcher.group()) : 0;
-            splitMap.put(matcher.group(), wordCount + 1);
-        }
-        return splitMap;
+        return Arrays.stream(word.split(" "))
+            .filter(s -> {
+                return !s.isEmpty();
+            })
+            .collect((Collectors.groupingBy(s -> s, Collectors.reducing(0, s -> 1, Integer::sum))));
+
+
+
+        //        Matcher matcher = pattern.matcher(word.toLowerCase());
+        //
+        //        while (matcher.find()) {
+        //            int wordCount = splitMap.get(matcher.group()) != null ? splitMap.get(matcher.group()) : 0;
+        //            splitMap.put(matcher.group(), wordCount + 1);
+        //        }
+        //        return splitMap;
     }
 }
